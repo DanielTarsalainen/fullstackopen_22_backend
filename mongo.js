00839ce537f2e75f9d6c2c2c07c5dbce575f34e3
Daniel Tarsalainen <daniel.tarsalainen@tietoevry.com>
2022-07-07 17:14:59 +0300

    Uncommented mongo.js file

--- mongo.js
+++ mongo.js
@@ -1,42 +1,43 @@
-// const mongoose = require("mongoose");
+/* eslint-disable no-undef */
+const mongoose = require("mongoose");
 
-// if (process.argv.length < 3) {
-//   console.log("give password as argument");
-//   process.exit();
-// }
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit();
+}
 
-// const password = process.argv[2];
+const password = process.argv[2];
 
-// const url = `mongodb+srv://fullstack22:${password}@cluster0.ovoum.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstack22:${password}@cluster0.ovoum.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
 
-// mongoose.connect(url);
+mongoose.connect(url);
 
-// const personSchema = new mongoose.Schema({
-//   name: String,
-//   number: String,
-// });
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
 
-// // Modelit ovat konstruktoriffunktioita, jotka luovat paramterien perusteell js olioita
-// const Person = mongoose.model("Person", personSchema);
+// Modelit ovat konstruktoriffunktioita, jotka luovat paramterien perusteell js olioita
+const Person = mongoose.model("Person", personSchema);
 
-// const person = new Person({
-//   name: process.argv[3],
-//   number: process.argv[4],
-// });
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+});
 
-// const argumentObj = process.argv;
+const argumentObj = process.argv;
 
-// if (argumentObj.length == 3) {
-//   console.log("phonebook:");
-//   Person.find({}).then((result) => {
-//     result.forEach((person) => {
-//       console.log(person.name + " " + person.number);
-//     });
-//     mongoose.connection.close();
-//   });
-// } else if (argumentObj.length == 5) {
-//   person.save().then((result) => {
-//     console.log("note saved!");
-//     mongoose.connection.close();
-//   });
-// }
+if (argumentObj.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person.name + " " + person.number);
+    });
+    mongoose.connection.close();
+  });
+} else if (argumentObj.length === 5) {
+  person.save().then(() => {
+    console.log("note saved!");
+    mongoose.connection.close();
+  });
+}
